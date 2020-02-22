20b7886ded90b4b9c5d20af91348233d477f7212
Captain-dikal <33635085+Captain-dikal@users.noreply.github.com>
2020-02-22 19:18:29 +0300

    Update bulma.js
    add livesearch

--- 2019/practic-course-4/twitter-cards/js/bulma.js
+++ 2019/practic-course-4/twitter-cards/js/bulma.js
@@ -1,3 +1,28 @@
+function sreach_func(){
+	var search= document.getElementsByTagName("input")[0];
+	var searchtext=search.value;
+	searchtext=searchtext.replace(/\s/g, '');
+	searchtext=searchtext.toLowerCase();
+	console.log(searchtext);
+	var allhtml = document.body.innerHTML;
+	
+	//(".column.is-one-third")
+	var tf="";
+	var sp="";
+	var elemf = document.getElementsByClassName("is-one-third");
+	for (var i = 0; i < elemf.length; i ++){
+		tf=elemf[i].innerText;
+		tf=tf.replace(/\s/g, '');
+		tf=tf.toLowerCase();
+		sp=tf.includes(searchtext);
+		console.log(sp);
+		if(sp==true){
+			elemf[i].style.display = 'block';
+		}else{
+			elemf[i].style.display = 'none';
+		}
+	}
+}
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 (function() {
